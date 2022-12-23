a0b1c3b727b65d42b2983570ab6cf67aaa4f0e48
Iryna <jakubenko.irina@gmail.com>
2022-12-23 15:22:38 +0200

    start first app Lection 19-practies

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-const number = 5;
-let a = 10;
-console.log (a);
+//const number = 5;
+//let a = 10;
+//console.log (a);
+
+let numberOfFilms = prompt("Сколько фильмов Вы уже посмотрели?");
+
+let personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+};
+
+let lastFilm = prompt("Один из последних просмотренных фильмов?");
+let markFilm = prompt("На сколько оцените его?");
+
+let movies = {
+    logan: lastFilm,
+    mark: markFilm
+};
+console.log(movies);
